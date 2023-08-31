ee149df193709a00544be8e26ac8cd5afbefecc7
Akim Vakhrenev <akeem3159119@gmail.com>
2023-08-31 21:30:55 +0300

    //e2e test

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -23,9 +23,9 @@ describe("Credit Card Validator form", () => {
     });
 
     browser = await puppetteer.launch({
-      headless: false, // show gui
-      slowMo: 250,
-      devtools: true, // show devTools
+      //headless: false, // show gui
+      //slowMo: 250,
+      //devtools: true, // show devTools
     });
     page = await browser.newPage();
   });
