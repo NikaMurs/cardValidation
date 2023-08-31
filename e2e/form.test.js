66ddb65af4c810410242b2ecd01b2401d5fe7903
Akim Vakhrenev <akeem3159119@gmail.com>
2023-08-31 20:18:15 +0300

    e2e disabled

--- e2e/form.test.js
+++ e2e/form.test.js
@@ -1,57 +1,59 @@
+/* eslint-disable jest/no-commented-out-tests */
 /* eslint-disable jest/expect-expect */
-import puppeteer from "puppeteer";
 
-describe("cardWidget test", () => {
-  let browser;
-  let page;
+// import puppeteer from "puppeteer";
 
-  beforeEach(async () => {
-    browser = await puppeteer.launch({
-      headless: true,
-      //slowMo: 100,
-      //devtools: true,
-    });
+// describe("cardWidget test", () => {
+//   let browser;
+//   let page;
 
-    page = await browser.newPage();
-  });
+//   beforeEach(async () => {
+//     browser = await puppeteer.launch({
+//       headless: true,
+//       //slowMo: 100,
+//       //devtools: true,
+//     });
 
-  test("cardWidget should render on page start", async () => {
-    await page.goto("http://localhost:9000");
+//     page = await browser.newPage();
+//   });
 
-    await page.waitForSelector(".card-validation-widget");
-  });
+//   test("cardWidget should render on page start", async () => {
+//     await page.goto("http://localhost:9000");
 
-  test("cardWidget should be add .valid class to title if card is valid", async () => {
-    await page.goto("http://localhost:9000");
+//     await page.waitForSelector(".card-validation-widget");
+//   });
 
-    await page.waitForSelector(".card-validation-widget");
+//   test("cardWidget should be add .valid class to title if card is valid", async () => {
+//     await page.goto("http://localhost:9000");
 
-    const widget = await page.$(".card-validation-widget");
-    const input = await widget.$(".card-form-input");
-    const button = await widget.$(".card-form-submit");
+//     await page.waitForSelector(".card-validation-widget");
 
-    await input.type("6011111111111117");
-    await button.click();
+//     const widget = await page.$(".card-validation-widget");
+//     const input = await widget.$(".card-form-input");
+//     const button = await widget.$(".card-form-submit");
 
-    await page.waitForSelector(".title.valid");
-  }, 10000);
+//     await input.type("6011111111111117");
+//     await button.click();
 
-  test("cardWidget should be add .invalid class to title if card is invalid", async () => {
-    await page.goto("http://localhost:9000");
+//     await page.waitForSelector(".title.valid");
+//   }, 10000);
 
-    await page.waitForSelector(".card-validation-widget");
+//   test("cardWidget should be add .invalid class to title if card is invalid", async () => {
+//     await page.goto("http://localhost:9000");
 
-    const widget = await page.$(".card-validation-widget");
-    const input = await widget.$(".card-form-input");
-    const button = await widget.$(".card-form-submit");
+//     await page.waitForSelector(".card-validation-widget");
 
-    await input.type("4111 1111 1111 1113");
-    await button.click();
+//     const widget = await page.$(".card-validation-widget");
+//     const input = await widget.$(".card-form-input");
+//     const button = await widget.$(".card-form-submit");
 
-    await page.waitForSelector(".title.invalid");
-  }, 10000);
+//     await input.type("4111 1111 1111 1113");
+//     await button.click();
 
-  afterEach(async () => {
-    await browser.close();
-  });
-});
+//     await page.waitForSelector(".title.invalid");
+//   }, 10000);
+
+//   afterEach(async () => {
+//     await browser.close();
+//   });
+// });
